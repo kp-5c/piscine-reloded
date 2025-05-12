a3332d43c448e8340bbda32ddead6a52e620220d
kp-5c <emma.benoist91@gmail.com>
2025-05-12 14:07:10 +0200

    Add files via upload

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -6,14 +6,14 @@
 /*   By: ebenoist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 14:18:04 by ebenoist          #+#    #+#             */
-/*   Updated: 2025/04/25 14:39:07 by ebenoist         ###   ########.fr       */
+/*   Updated: 2025/04/28 09:36:39 by ebenoist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_foreach(int *tab, int length, void (*f)(int))
 {
 	int	i;
-	
+
 	i = 0;
 	while (i < length)
 	{
